5b97160a2355b703c1c36e54e62ca04d214bd678
ioCron <info@iocron.com>
2016-09-28 00:25:41 +0200

    Application Restructuring & Fixes

--- deploy.sh
+++ deploy.sh
@@ -1,54 +1,48 @@
 #!/bin/bash
 
-##########################
-#    Bash Strict Mode    #
-##########################
+### Bash Strict Mode ###
 set -eu
 IFS=$'\n\t'
 
-##########################
-#   Check Permissions    #
-##########################
-if (( $EUID != 0 )); then
-    printf "Please run this script as root."; exit 1;
-fi
-
-##########################
-#    Setup Variables     #
-##########################
+### Setups Variables ###
 if (( -f `dirname $BASH_SOURCE`/config.cnf )); then 
 	source `dirname $BASH_SOURCE`/config.cnf;
 else
-	printf "${NOW_EXT}: ${RED}ERROR: Please make sure a configuration file (config.cnf) is set.${NORMAL}" >> $ERROR_LOG; exit 1;
+	printf "$(date +"%Y-%m-%d-%M%S") [ERROR]: Please make sure a configuration file (config.cnf) is set." >> `dirname $BASH_SOURCE`/logs/error.log; exit 1;
 fi
 
-##########################
-#       Deployment       #
-##########################
+### Check Requirements ###
+if (( $EUID != 0 )); then
+    printf "${MSG_ERROR} Please run this script as root.${MSG_NORMAL}" >> $ERROR_LOG; exit 1;
+fi
 
-printf "###############################\n#    Deployment in progress..    #\n###############################\n";
+if ! hash plesk 2>/dev/null; then
+	printf "${MSG_ERROR} Plesk is not installed on your System.${MSG_NORMAL}" >> $ERROR_LOG; exit 1; 
+fi
+
+printf "###############################\n#      Deployment in progress..     #\n###############################\n";
 printf "Init..\n";
 
-printf "###############################\n#    Custom Bash Profiles Init    #\n###############################\n";
+printf "\n###############################\n#    Custom Bash Profiles Init    #\n###############################\n";
 if (( $BASH_PROFILE_DEFAULT == 1 || $BASH_PROFILE_CUSTOM == 1 )); then
 	if (( -f ~/.bash_profile )); then
 		sed -i -e '/### BASH_PROFILE_DEFAULT ###/,/### BASH_PROFILE_DEFAULT ###/d' ~/.bash_profile
 		sed -i -e '/### BASH_PROFILE_CUSTOM ###/,/### BASH_PROFILE_CUSTOM ###/d' ~/.bash_profile
 	else
-		printf "${NOW_EXT}: ${YELLOW}WARNING: File ~/.bash_profile wasn't found on your system. A new ~/.bash_profile will be created from your config.${NORMAL}" >> 
+		printf "${MSG_WARNING} File ~/.bash_profile wasn't found on your system. A new ~/.bash_profile will be created from your config.${MSG_NORMAL}" >> $ERROR_LOG;
 	fi
 	
-	if (( $BASH_PROFILE_DEFAULT == 1 )); then cat $SCRIPTPATH/configs/bash_profile.cnf >> ~/.bash_profile; fi
-	if (( $BASH_PROFILE_CUSTOM == 1 )); then cat $SCRIPTPATH/configs_custom/bash_profile.cnf >> ~/.bash_profile; fi
+	if (( $BASH_PROFILE_DEFAULT == 1 )); then cat $CONFIGS_DEFAULT/bash_profile.cnf >> ~/.bash_profile; fi
+	if (( $BASH_PROFILE_CUSTOM == 1 )); then cat $CONFIGS_CUSTOM/bash_profile.cnf >> ~/.bash_profile; fi
 	
 	source ~/.bash_profile
 	
-	printf "${GREEN}FINISHED: The bash profiles have been successfully applied / added to ~/.bash_profile.${NORMAL}";
+	printf "${MSG_DONE} The bash profiles have been successfully applied / added to ~/.bash_profile.${MSG_NORMAL}";
 else
-	printf "${UNDERLINE}INFO: No Bash Profile Configuration in your config.cnf selected, skip..${NORMAL}";
+	printf "${MSG_INFO} No Bash Profile Configuration in your config.cnf selected, skip..${MSG_NORMAL}";
 fi
 
-printf "###############################\n#    Additional Linux Packages    #\n###############################\n";
+printf "\n###############################\n#    Additional Linux Packages    #\n###############################\n";
 if (( "${#DISTRO}" > 0 )); then
 	if (( $DISTRO =~ "Ubuntu" || $DISTRO =~ "Debian" )); then 
 		apt-get -y install $LINUX_PACKAGES
@@ -57,29 +51,30 @@ if (( "${#DISTRO}" > 0 )); then
 		yum -y install $LINUX_PACKAGES
 		LINUX_INSTALL_PCKGS=1
 	else
-		printf "${NOW_EXT}: ${YELLOW}WARNING: Wasn't able to determine your Distro Type (e.g. CentOS, Ubuntu), therefor no linux packages have been installed.${NORMAL}" >> $ERROR_LOG;
+		printf "${MSG_WARNING} Wasn't able to determine your Distro Type (e.g. CentOS, Ubuntu), therefor no linux packages have been installed.${MSG_NORMAL}" >> $ERROR_LOG;
 	fi
 fi
 
 if (( $LINUX_INSTALL_PCKGS == 1 )); then
-	printf "${GREEN}FINISHED: Installed the additional linux packages $LINUX_PACKAGES (please see the install process above to check if everything has been installed successfully).${NORMAL}";
+	printf "${MSG_DONE} Installed the additional linux packages $LINUX_PACKAGES (please see the install process above to check if everything has been installed successfully).${MSG_NORMAL}";
 else
-	printf "${UNDERLINE}INFO: No Linux Packages Selected.${NORMAL}"
+	printf "${MSG_INFO} No Linux Packages Selected.${MSG_NORMAL}"
 fi
 
-printf "###############################\n#     Additional Nginx Conf's     #\n###############################\n";
+printf "\n###############################\n#     Additional Nginx Conf's     #\n###############################\n";
 if (( $NGINX_GZIP == 1 )); then
 	rm -f /etc/nginx/conf.d/gzip.conf
-	cp $SCRIPTPATH/configs/nginx_gzip.cnf /etc/nginx/conf.d/gzip.conf
-	printf "${GREEN}FINISHED: Copied ${SCRIPTPATH}/configs/nginx_gzip.cnf to /etc/nginx/conf.d/gzip.conf${NORMAL}"
+	cp $CONFIGS_DEFAULT/nginx_gzip.cnf /etc/nginx/conf.d/gzip.conf
+	printf "${MSG_DONE} Copied ${SCRIPTPATH}/configs/default/nginx_gzip.cnf to /etc/nginx/conf.d/gzip.conf${MSG_NORMAL}"
 else
-	printf "${UNDERLINE}INFO: Skipped nginx gzip configuration..${NORMAL}"
+	printf "${MSG_INFO} Skipped nginx gzip configuration..${MSG_NORMAL}"
 fi
 
-printf "###############################\n#      Import Custom Scripts      #\n###############################\n";
+printf "\n###############################\n#      Import Custom Scripts      #\n###############################\n";
 if (( ! -d ~/bin )); then mkdir ~/bin; fi
 /bin/cp -f $SCRIPTPATH/bin/* ~/bin
 chmod 700 ~/bin/*
-printf "${GREEN}FINISHED: All custom scripts have been copied to ~/bin you can call a script with yourscript.sh from anywhere.${NORMAL}";
+printf "${MSG_DONE} All custom scripts have been copied to ~/bin you can call a script with yourscript.sh from anywhere.${MSG_NORMAL}";
 
-
+printf "\n###############################\n#       Deployment Finished       #\n###############################\n";
+printf "${MSG_DONE} The plesk deployer has finished your deployment.${MSG_NORMAL}";
