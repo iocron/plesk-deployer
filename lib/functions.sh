c6dfe29cd2962b63f4d3c132d9edd6978385cd24
ioCron <info@iocron.com>
2016-09-29 05:32:02 +0200

    Added comments / docs to functions.sh

--- lib/functions.sh
+++ lib/functions.sh
@@ -1,4 +1,8 @@
-### getConfig() ###
+### getConfig() - Usage: getConfig <configFileName> ###
+# Usage: getConfig <configFileName>
+# Usage Example: getConfig nginx_gzip.cnf
+# Parameter: <configFileName> Can be any name that is available in configs/default or configs/custom
+# Meaning: Outputs/Returns the Path to the right config file depending on the config.cnf settings
 function getConfig(){
 	if [[ -n "$1" ]]; then
 		if [[ $CONFIGS_CUSTOM == 1 && -f $CONFIGS_PATH_CUSTOM/$1 ]]; then
@@ -16,11 +20,16 @@ function getConfig(){
 }
 
 ### syslogger() ###
+# Usage: syslogger <type> <message>
+# Usage Example: syslogger "ERROR" "This is a Error Message."
+# Parameter: <type> Can be ERROR, WARNING, INFO or DONE
+# Parameter: <message> Just a normal Text to Inform the User
+# Meaning: Logs or Outputs/Returns specific Messages (and outputs them to a file if it is a error)
 function syslogger(){
 	if [[ ! ${2+x} ]]; then
 		printf "${RED}${MSG_ERROR} syslogger() Parameters missing. E.g. syslogger <type> <message>\n(possible types are: ${MSG_TYPES}) ${MSG_RESET}\n"; printf "$(currentTime) [ERROR]: syslogger() Parameters missing.\n" >> $ERROR_LOG; exit 1;
 	fi
-	
+
 	case "$1" in
 		"ERROR") printf "${RED}${MSG_ERROR} $2 ${MSG_RESET}\n"; printf "$(currentTime) [ERROR]: $2 \n" >> $ERROR_LOG; exit 1;;
 		"WARNING") printf "${YELLOW}${MSG_WARNING} $2 ${MSG_RESET}\n"; printf "$(currentTime) [WARNING]: $2 \n" >> $ERROR_LOG;;
@@ -31,11 +40,15 @@ function syslogger(){
 }
 
 ### currentTime() ###
+# Usage: currentTime
+# Meaning: Outputs/Returns the current Time
 function currentTime(){
 	printf $(date +"$TIME_FORMAT");
 }
 
 ### currentTimeFile() ###
+# Usage: currentTimeFile
+# Meaning: Outputs/Returns the current Time as a valid File Format
 function currentTimeFile(){
 	printf $(date +"$TIME_FORMAT_FILE");
 }
