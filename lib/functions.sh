6e1abdb144ad21114f311707ffc304e06b68debc
ioCron <info@iocron.com>
2016-10-10 05:28:02 +0200

    Improved sysLogger & mailAdmin
    - Writes more informations in the deployment logging
    - Better adminMail sending between normal Messages and Deployment Logs

--- lib/functions.sh
+++ lib/functions.sh
@@ -27,16 +27,24 @@ function getConfig(){
 # Meaning: Outputs/Returns specific Messages (and outputs them to a file if it's a "ERROR" type)
 function sysLogger(){
 	# Check if the Function has been properly accessed
-	if [[ ! ${2+x} ]]; then
+	if [[ ! ${1+x} && ! ${2+x} ]]; then
 		printf "${RED}${MSG_ERROR} sysLogger() Parameters missing. E.g. sysLogger <type> <message>\n(possible types are: ${MSG_TYPES}) ${MSG_RESET}\n"; printf "$(currentTime) [$1]: sysLogger() Parameters missing.\n" >> $LOG_ERROR; exit 1;
 	fi
 
-	# Write Log Deployment
-	printf "$2\n" >> $LOG_DEPLOYMENT;
+	# Write Deployment Logging
+	# $PD_ADMIN_MAIL != 0 && $PD_ADMIN_MAIL_SEND_LOG != 0
+	# if [[ $PD_ADMIN_MAIL_SEND_LOG == "$1" || $PD_ADMIN_MAIL_SEND_LOG == "ALL" ]]; then
+	if [[ $PD_LOG_DEPLOYMENT != 0 ]]; then
+		if [[ "$1" == "TEXT" ]]; then
+			printf "$2" >> $LOG_DEPLOYMENT;
+		else
+			printf "[$1]: $2\n" >> $LOG_DEPLOYMENT;
+		fi
+	fi
 
-	# SysLogger Message Output & Error Logging
+	# Write Error Logging & Output Messages
 	case "$1" in
-		"ERROR") printf "\n${RED}${MSG_ERROR} $2 ${MSG_RESET}\n"; mailAdmin "$1" "$2"; printf "$(currentTime) [$1]: $2 \n" >> $LOG_ERROR; exit 1;;
+		"ERROR") printf "\n${RED}${MSG_ERROR} $2 ${MSG_RESET}\n"; if [[ $PD_ADMIN_MAIL_SEND_LOG == "LOG_ERROR" ]]; then mailAdmin "$1" "$2"; fi; printf "$(currentTime) [$1]: $2 \n" >> $LOG_ERROR; exit 1;;
 		"WARNING") printf "\n${YELLOW}${MSG_WARNING} $2 ${MSG_RESET}\n"; printf "$(currentTime) [$1]: $2 \n" >> $LOG_ERROR;;
 		"INFO") printf "\n${UNDERLINE}${MSG_INFO} $2 ${MSG_RESET}\n";;
 		"DONE") printf "\n${GREEN}${MSG_DONE} $2 ${MSG_RESET}\n";;
@@ -46,11 +54,21 @@ function sysLogger(){
 }
 
 ### mailAdmin ###
-# Usage: mailAdmin <subject> <yourMessage>
-# Example: mailAdmin "ERROR" "Your Message"
+# Usage: mailAdmin OR mailAdmin <subject> <yourMessage>
+# Example1: mailAdmin # Sends all Logs (Log Deployment) or no Logs to the Admin (based on the config settings)
+# Example2: mailAdmin "A Error occurred" "Your Message.." # Send only a specific Message to the Admin
 function mailAdmin(){
 	if [[ $PD_ADMIN_MAIL != 0 && "${#PD_ADMIN_MAIL}" > 0 ]]; then
-		echo "$2" | mail -s "Plesk Deployer - $1" "$PD_ADMIN_MAIL";
+		if [[ ! ${1+x} && ! ${2+x} ]]; then
+			echo "$2" | mail -s "Plesk Deployer - $1" "$PD_ADMIN_MAIL";
+		elif [[ $PD_ADMIN_MAIL_SEND_LOG == "LOG_DEPLOYMENT" && $PD_LOG_DEPLOYMENT == 1 ]]; then
+			sysLogger "DONE" "A Email with the content of the Log Deployment will be sent to ${PD_ADMIN_MAIL}.";
+			mail -s "Plesk Deployer - Deployment Log (deployment_$TIME_CURRENT_FILE.log)" "$PD_ADMIN_MAIL" < $LOG_DEPLOYMENT
+		else
+			sysLogger "WARNING" "Wrong Admin Mail Log type specified or no Log type specified at all (see PD_ADMIN_MAIL, PD_ADMIN_MAIL_SEND_LOG and maybe PD_LOG_DEPLOYMENT as well)";
+		fi
+	else
+		sysLogger "INFO" "The mailAdmin functionality isn't activated, skip..";
 	fi
 }
 
