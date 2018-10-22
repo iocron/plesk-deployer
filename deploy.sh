34ba55cff529dee5f3d9cb0661e4a653e715d4c9
ioCron <info@iocron.com>
2018-10-22 19:33:05 +0200

    Check if aum command exists

--- deploy.sh
+++ deploy.sh
@@ -282,8 +282,10 @@ if [[ $PLESK_MODSECURITY_FIREWALL == 1 ]]; then
 		sysLogger "INFO" "The Web Application Firewall (ModSecurity) is already activated (Ruleset: ${PLESK_MODSECURITY_FIREWALL_RULESET}) (skip).";
 	else
 		sysLogger "TEXT" "Activating Web Application Firewall (ModSecurity) with Ruleset \"${PLESK_MODSECURITY_FIREWALL_RULESET}\"..\n";
-		if [[ $PLESK_MODSECURITY_FIREWALL_RULESET == "tortix" ]]; then
+		if [[ $PLESK_MODSECURITY_FIREWALL_RULESET == "tortix" && "$(type aum 2>/dev/null)" ]]; then
 			aum -u; # Atomic ModSecurity Updater
+		else
+			sysLogger "WARNING" "Command aum not found (skip).";
 		fi
 		plesk bin server_pref --update-web-app-firewall -waf-rule-engine on -waf-rule-set $PLESK_MODSECURITY_FIREWALL_RULESET | tee -a $LOG_DEPLOYMENT;
 		plesk sbin modsecurity_ctl --disable | tee -a $LOG_DEPLOYMENT;
